e55fa319e16e10cdd476278a19b3d61d223d703c
Chris <karampagiaschris@gmail.com>
2022-09-25 13:30:18 +0300

    Add backspace functionality to calculator

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const operators = document.querySelectorAll(".operators-container .btn");
 const decimal = document.querySelector(".dec");
 const equality = document.querySelector(".equality");
 const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".backspace")
 let operationsDisplay = '';
 let operationSymbol = '';
 let operationResult = 0;
 let operateCounter = 0;
 let a = '';
 let b = '';
+let lastOperation = ['','',''];
+let backspaceCounter = 0;
 numbers.forEach(number => {
     number.addEventListener("click", (e) => {
         if (screenOperations.textContent == "*" || screenOperations.textContent == "+" || screenOperations.textContent == "-" || screenOperations.textContent == "÷"){
@@ -22,13 +25,16 @@ numbers.forEach(number => {
             operationSymbol = "";   
             screenResult.textContent = "Error";
         }else{
+            backspaceCounter = 0;
             if (screenResult.textContent != "") {
                 screenResult.textContent = "";
             }
             if (operationSymbol == ""){
                 a += e.target.textContent;
+                lastOperation[0] += e.target.textContent;
             }else if (operationSymbol != "") {
                 b += e.target.textContent;
+                lastOperation[2] += e.target.textContent;
             }
             screenOperations.textContent = operationsDisplay + e.target.textContent;
             operationsDisplay += e.target.textContent;;
@@ -36,9 +42,7 @@ numbers.forEach(number => {
 
 operators.forEach(operator => {
     operator.addEventListener("click",(e) => {
-        if (screenResult.textContent != "") {
-            screenResult.textContent = "";
-        }
+        if (screenResult.textContent != "") screenResult.textContent = "";
 
         if (screenOperations.textContent[screenOperations.textContent.length -1] == "*" || screenOperations.textContent[screenOperations.textContent.length -1] == "+" || screenOperations.textContent[screenOperations.textContent.length -1] == "-" || screenOperations.textContent[screenOperations.textContent.length -1] == "÷" ){
             operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 1);
@@ -47,29 +51,34 @@ operators.forEach(operator => {
             operationSymbol = operationSymbol.slice(0 , operationSymbol.length - 1);
             operationSymbol += e.target.textContent;
         }else {
+            backspaceCounter = 0;
             screenOperations.textContent = operationsDisplay + e.target.textContent;
             operationsDisplay += e.target.textContent;
             operationSymbol += e.target.textContent;
+            lastOperation[1] += e.target.textContent;
             if (a != "" && b != "" ) {   
                 if (b != "0"){                                      
-                let result = operate(operationSymbol[0],parseFloat(a),parseFloat(b))
-                result = Math.round(result * 100) / 100;
-                operationResult = result;
-                operationSymbol = operationSymbol.slice(-1);
-                a = String(result);
-                b = "";
-                }else {
-                    screenResult.textContent = "Division by zero is a crime against math!";
+                    let result = operate(operationSymbol[0],parseFloat(a),parseFloat(b))
+                    result = Math.round(result * 100) / 100;
+                    operationResult = result;
+                    lastOperation[0] = a;
+                    lastOperation[1] = operationSymbol[0];
+                    lastOperation[2] = b;
+                    a = String(result);
                     b = "";
-                    operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 2);
+                    operationSymbol = operationSymbol.slice(-1);
+                }else {
+                    alert("Division by zero is undefined");
+                    b = "";
+                    operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 1);
                     screenOperations.textContent = operationsDisplay;
-                    operationSymbol = "";
                 }
             }
         }
 }) });
 
 decimal.addEventListener("click", (e) =>{
+    backspaceCounter = 0;
     if (operationSymbol == ""){
         if (!a.includes(".")) {
             a += e.target.textContent;
@@ -85,22 +94,28 @@ decimal.addEventListener("click", (e) =>{
     }
 })
 equality.addEventListener("click", () => {
-    if (a != "" && b != "") {   
+    if (screenOperations.textContent[screenOperations.textContent.length -1] == "*" || screenOperations.textContent[screenOperations.textContent.length -1] == "+" || screenOperations.textContent[screenOperations.textContent.length -1] == "-" || screenOperations.textContent[screenOperations.textContent.length -1] == "÷" ){
+        screenResult.textContent = "Malformed expression";
+        return;
+    }else if (a != "" && b != "") {   
+        backspaceCounter = 0;
         if (b != "0"){                                      
-        let result = operate(operationSymbol,parseFloat(a),parseFloat(b))
-        result = Math.round(result * 100) / 100;
-        operationResult = result;
-        operationSymbol = operationSymbol.slice(-1);
-        a = String(result);
-        b = "";
-        screenResult.textContent = String(operationResult);
-        operationSymbol = "";
-        }else {
-            screenResult.textContent = "Division by zero is a crime against math!";
+            let result = operate(operationSymbol,parseFloat(a),parseFloat(b))
+            result = Math.round(result * 100) / 100;
+            operationResult = result;
+            lastOperation[0] = a;
+            lastOperation[1] = operationSymbol;
+            lastOperation[2] = b;
+            operationSymbol = operationSymbol.slice(-1);
+            a = String(result);
             b = "";
-            operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 2);
-            screenOperations.textContent = operationsDisplay;
+            screenResult.textContent = String(operationResult);
             operationSymbol = "";
+        }else {
+            alert("Division by zero is undefined");
+            b = "";
+            operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 1);
+            screenOperations.textContent = operationsDisplay;
         }
     }else if (a == "" || operateCounter == 0){
         screenResult.textContent = "Error";
@@ -110,18 +125,72 @@ equality.addEventListener("click", () => {
     }
 );
 
-clear.addEventListener("click", (e) => {
+clear.addEventListener("click", () => {
      operationsDisplay = '';
      screenResult.textContent = "";
      screenOperations.textContent = "";
      operationSymbol = '';
      operationResult = 0;
+     previousOperationResult = 0;
      operateCounter = 0;
      a = '';
      b = '';
 })
 
-
+backspace.addEventListener("click", () => {
+    backspaceCounter++ ;
+    if (backspaceCounter <= 2){
+        a = lastOperation[0];
+        operationSymbol = lastOperation[1];
+        b = lastOperation[2];
+        if (screenResult.textContent != "" && operateCounter == 1){
+            screenResult.textContent = "";
+            a = lastOperation[0];
+            operationSymbol = lastOperation[1];
+            b = lastOperation[2];
+            operateCounter--;
+            operationResult = 0;
+        }else if (screenResult.textContent != "" && operateCounter > 1){
+            screenResult.textContent = "";
+            a = lastOperation[0];
+            operationSymbol = lastOperation[1];
+            b = lastOperation[2];
+            operateCounter--;
+            operationResult = Number(lastOperation[0]);
+        }else if ((screenOperations.textContent[screenOperations.textContent.length -1] == "*" || screenOperations.textContent[screenOperations.textContent.length -1] == "+" || screenOperations.textContent[screenOperations.textContent.length -1] == "-" || screenOperations.textContent[screenOperations.textContent.length -1] == "÷") && b!="" && operateCounter == 1){
+            a = lastOperation[0];
+            operationSymbol = lastOperation[1];
+            b = lastOperation[2];
+            operateCounter--;
+            operationResult = 0;
+            screenOperations.textContent = screenOperations.textContent.slice(0,screenOperations.textContent.length - 1);
+            operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 1);
+        }else if ((screenOperations.textContent[screenOperations.textContent.length -1] == "*" || screenOperations.textContent[screenOperations.textContent.length -1] == "+" || screenOperations.textContent[screenOperations.textContent.length -1] == "-" || screenOperations.textContent[screenOperations.textContent.length -1] == "÷") && b!="" && operateCounter > 1){
+            a = lastOperation[0];
+            operationSymbol = lastOperation[1];
+            b = lastOperation[2];
+            operateCounter--;
+            operationResult = Number(lastOperation[0]);
+            screenOperations.textContent = screenOperations.textContent.slice(0,screenOperations.textContent.length - 1);
+            operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 1);
+        }else if ((screenOperations.textContent[screenOperations.textContent.length -1] != "*" || screenOperations.textContent[screenOperations.textContent.length -1] != "+" || screenOperations.textContent[screenOperations.textContent.length -1] != "-" || screenOperations.textContent[screenOperations.textContent.length -1] != "÷") && b != ""){
+            lastOperation[2] = lastOperation[2].slice(0,lastOperation[2].length - 1);
+            b = lastOperation[2];
+            screenOperations.textContent = screenOperations.textContent.slice(0,screenOperations.textContent.length - 1);
+            operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 1);
+        }else if ((screenOperations.textContent[screenOperations.textContent.length -1] == "*" || screenOperations.textContent[screenOperations.textContent.length -1] == "+" || screenOperations.textContent[screenOperations.textContent.length -1] == "-" || screenOperations.textContent[screenOperations.textContent.length -1] == "÷") && b == "" && operateCounter == 0){
+            lastOperation[1] = lastOperation[1].slice(0, lastOperation[1].length - 1);
+            operationSymbol = lastOperation[1];
+            screenOperations.textContent = screenOperations.textContent.slice(0,screenOperations.textContent.length - 1);
+            operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 1);
+        }else if ((screenOperations.textContent[screenOperations.textContent.length -1] != "*" || screenOperations.textContent[screenOperations.textContent.length -1] != "+" || screenOperations.textContent[screenOperations.textContent.length -1] != "-" || screenOperations.textContent[screenOperations.textContent.length -1] != "÷") && b == "" && operateCounter == 0 ){
+            lastOperation[0] = lastOperation[0].slice(0, lastOperation[0].length - 1);
+            a = lastOperation[0];
+            screenOperations.textContent = screenOperations.textContent.slice(0,screenOperations.textContent.length - 1);
+            operationsDisplay = operationsDisplay.slice(0,operationsDisplay.length - 1);
+        }
+    }else return;
+})
 
 
 function add(a,b) {
@@ -159,4 +228,3 @@ function operate(operator,a,b) {
     }
     return result;
 }
-
